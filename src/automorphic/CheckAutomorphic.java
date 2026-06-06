d972d55accd37094d3cecd27857c028ee67fd425
imsydulu <tsydulu9@gmail.com>
2026-06-06 19:44:39 +0530

    Automorphic number number task

---
+++ src/automorphic/CheckAutomorphic.java
@@ -0,0 +1,28 @@
+package automorphic;
+
+public class CheckAutomorphic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		checkAutomorphic();
+	}
+
+	static void checkAutomorphic() {
+		int val = 25;
+		int sqroot = 0;
+		int newVal = 0;
+		while (val != 0) {
+			sqroot = (int) Math.pow(val, 2);
+
+			int lastDigit = sqroot % 10;
+			int secondDigit = (sqroot / 10) % 10;
+			newVal = secondDigit * 10 + lastDigit;
+			break;
+
+		}
+		System.out.println(newVal);
+		System.out.println(newVal == val ? "Automorphic" : "Not a Automorphic");
+
+	}
+
+}
