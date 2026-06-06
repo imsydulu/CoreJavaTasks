5e6395b2418da9407eae74106754ca93a2db14bd
imsydulu <tsydulu9@gmail.com>
2026-06-06 19:50:11 +0530

    automorphic different aproach

--- src/automorphic/CheckAutomorphic.java
+++ src/automorphic/CheckAutomorphic.java
@@ -4,7 +4,7 @@ public class CheckAutomorphic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		checkAutomorphic();
+		checkAutomorphic1();
 	}
 
 	static void checkAutomorphic() {
@@ -13,14 +13,28 @@ public class CheckAutomorphic {
 		int newVal = 0;
 		while (val != 0) {
 			sqroot = (int) Math.pow(val, 2);
-
 			int lastDigit = sqroot % 10;
 			int secondDigit = (sqroot / 10) % 10;
 			newVal = secondDigit * 10 + lastDigit;
 			break;
 
 		}
-		System.out.println(newVal);
+		System.out.println(newVal == val);
+		System.out.println(newVal == val ? "Automorphic" : "Not a Automorphic");
+
+	}
+
+	static void checkAutomorphic1() {
+		int val = 25;
+		int sqroot = 0;
+		int newVal = 0;
+		while (val != 0) {
+			sqroot = (int) Math.pow(val, 2);
+			newVal = sqroot % 100;
+			break;
+
+		}
+		System.out.println(newVal == val);
 		System.out.println(newVal == val ? "Automorphic" : "Not a Automorphic");
 
 	}
